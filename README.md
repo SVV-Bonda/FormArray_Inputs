158a1a7648545eb3396312f1db68851e5c815b45
SVV-Bonda <122274519+SVV-Bonda@users.noreply.github.com>
2023-06-15 11:07:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FormArray_Inputs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-hp4axm)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, VERSION } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  constructor(private fb : FormBuilder){}
+
+  form = this.fb.group({
+    inputs : this.fb.array([])
+  })
+
+
+  get inputs() {
+    return this.form.get('inputs') as FormArray;
+  }
+
+  Inputadd() {
+    this.inputs.push(this.fb.control(''));
+  }
+
+  Inputremove(i: any) {
+    this.inputs.removeAt(i);
+  }
+}
